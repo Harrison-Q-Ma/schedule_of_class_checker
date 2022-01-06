4e85117fb43ca0f32746a5992bd47ede8e75bc97
Qixuan "Harrison" Ma <maq@mssm.org>
2022-01-05 23:00:03 -0800

    implemented email notif with Twilio

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import numpy as np
 import time
+import re
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 meeting_types = ['DI', 'LE', 'LA', 'ST', 'SE', 'AC', 'CL', 'FI', 'FM',
                  'FW', 'IN', 'IT', 'MU', 'OT', 'PB', 'PR', 'RE']
 
 
 def scrape(driver, class_name):
+    status = []
     for name in class_name:
         driver.get("https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudent.htm")
         # click on 'by code(s)'
@@ -51,18 +56,44 @@ def scrape(driver, class_name):
 
         if avail_seats > 0:
             print(f'There are {int(avail_seats)} seats left in {name}.')
+            status.append('Not Full')
         else:
             print(f'There is no remaining seat left in {name}.')
+            status.append('Full')
 
         element = driver.find_element_by_link_text('Start a new search')
         element.click()
+
+    return status
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 codes = input("Enter class codes, separated by commas, followed by ENTER: ")
 codes = codes.split(sep=',')
 
-while True:
-    scrape(driver, codes)
-    time.sleep(60)
+email = input('Enter email address you would like notifications to be sent to, followed by ENTER: ')
+has_email = False
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+if re.fullmatch(regex, email):
+    has_email = True
 
+init_status = scrape(driver, codes)
+
+while True:
+    time.sleep(60)
+    update_status = scrape(driver, codes)
+    if init_status != update_status:
+        message = Mail(
+            from_email='qhmaservices@gmail.com',
+            to_emails=email,
+            subject='Changes in Class Watchlist',
+            html_content=f'Current status for {codes} is {update_status}')
+        try:
+            sg = SendGridAPIClient('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')
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+    init_status = update_status
